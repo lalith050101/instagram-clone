bcd731b4902f5527d0bf22385f284dc6d251a75e
unknown <shakthibala499@gmail.com>
2022-03-24 14:16:13 +0530

    explore over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExploreComponent } from './main/explore/explore.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExploreComponent
   ],
   imports: [
     BrowserModule,
