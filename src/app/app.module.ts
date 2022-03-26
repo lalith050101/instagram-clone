5db662ef700c5f6f8afa6552ac0e336fc3c425b8
unknown <shakthibala499@gmail.com>
2022-03-26 07:59:32 +0530

    home page initiated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './main/home/home-component/home-component.component';
+import { PostComponentComponent } from './main/home/post-component/post-component.component';
+import { StoryComponentComponent } from './main/home/story-component/story-component.component';
+import { SuggestionComponentComponent } from './main/home/suggestion-component/suggestion-component.component';
+import {IvyCarouselModule} from 'angular-responsive-carousel';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    PostComponentComponent,
+    StoryComponentComponent,
+    SuggestionComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IvyCarouselModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
