31551af3f842f0df26b39e46378813018cd0b7b2
YADLAPALLI VENKATA LALITH KUMAR <yvlalithkumar@gmail.com>
2022-03-29 09:20:41 +0530

    update routing and home page

--- src/app/main/home/post-component/post-component.component.ts
+++ src/app/main/home/post-component/post-component.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/core/interfaces/post/post';
 import { PostService } from 'src/app/core/services/post/post.service';
 
 @Component({
@@ -8,9 +10,16 @@ import { PostService } from 'src/app/core/services/post/post.service';
 })
 export class PostComponentComponent implements OnInit {
 
-  constructor(private postservice:PostService) { }
+  constructor(private postservice:PostService, private http: HttpClient) { }
+
+  posts: any = [];
 
   ngOnInit(): void {
+    this.http.get("assets/static-data/posts.json").subscribe(data => {
+      console.log(data);
+      this.posts = data;
+      
+    })
   }
 
   postView(){
